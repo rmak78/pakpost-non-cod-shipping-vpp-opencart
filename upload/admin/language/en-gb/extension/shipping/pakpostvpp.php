c22372330642733f41d7a58846db2ffecbbe32bb
Rana Mansoor Akbar Khan <rmak@sutlej.net>
2021-09-30 06:42:14 +0500

    Add files via upload
    New Files for Vers. 1

---
+++ upload/admin/language/en-gb/extension/shipping/pakpostvpp.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'PakPost Registered Post (Cash Only)';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified PakPost Registered Post (CASH ONLY) shipping!';
+$_['text_edit']        = 'Edit PakPost Registered Post Shipping';
+
+// Entry
+$_['entry_rate']       = 'Rates';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc..';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify PakPost Registered Post(CASH ONLY)  shipping!';
